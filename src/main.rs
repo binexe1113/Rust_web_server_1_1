bdd90787f34376dae3daf9c0bea4d4d539db7ecb
Fabio Henrique Lopes <fabiohblopes3@gmail.com>
2025-08-18 19:54:03 -0300

    Initializes basic web server structure
    Sets up a basic web server with essential components.
    
    This commit introduces the foundational structure for a simple web server.
    It includes the main entry point, server setup, request handling, and method enumeration.
    
    The server currently listens on a specified address and prints a message
    to the console when it starts.
    
    Adds .gitignore and Cargo files.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+struct Server{
+    addr: String,
+}
+
+impl Server{
+    //Constructor for struct Server
+    fn new(addr: String)-> Self{
+        Server {
+            addr 
+        }
+    }
+
+    //self==this, self takes ownership of struct 
+    fn run(self){
+        println!("Listening on {}", self.addr);
+    }
+}
+
+struct Request{
+    path: String,
+    query_string: Option<String>, //Option cause could be null(Absent value)
+    method: Method,
+}
+
+enum Method{
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
